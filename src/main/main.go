ec59536f8d69483aea1c985bd72a4677c058d7ae
dwibagus154 <dwibagus154@gmail.com>
2021-04-03 20:59:35 +0700

    cara akses web

--- src/main/main.go
+++ src/main/main.go
@@ -1,34 +1,48 @@
 package main
 
 import (
-	"fmt"
-
-	"example.com/graph"
+	"log"
+	"net/http"
+	// "example.com/graph"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
-	fmt.Println("Selamat datang di aplikasi A*")
-	fmt.Print("Masukkan nama  file : ")
-	var fileName string
-	fmt.Scanln(&fileName)
-	graf := graph.ReadFile(fileName)
-	nodes := graf.GetNodes()
-	for true {
-		fmt.Print("Tampilkan Daftar Nama Simpul ? (y/n)")
-		var command string
-		fmt.Scanln(&command)
-		if command == "y" {
-			fmt.Println("Daftar Nama Simpul :")
-			graph.PrintListNodes(nodes)
-		}
-		fmt.Print("Masukkan Nama Posisi AWal : ")
-		var start string
-		fmt.Scanln(&start)
-		fmt.Print("Masukkan Nama Posisi Tujuan : ")
-		var goal string
-		fmt.Scanln(&goal)
-		fmt.Println(graf.Astar(start, goal))
-	}
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/Hello", helloHandler)
+
+	log.Println("starting port 8080")
+
+	err := http.ListenAndServe(":8080", mux)
+
+	log.Fatal(err)
+
+	// fmt.Println("Hello, World!")
+	// fmt.Println("Selamat datang di aplikasi A*")
+	// fmt.Print("Masukkan nama  file : ")
+	// var fileName string
+	// fmt.Scanln(&fileName)
+	// graf := graph.ReadFile(fileName)
+	// nodes := graf.GetNodes()
+	// for true {
+	// 	fmt.Print("Tampilkan Daftar Nama Simpul ? (y/n)")
+	// 	var command string
+	// 	fmt.Scanln(&command)
+	// 	if command == "y" {
+	// 		fmt.Println("Daftar Nama Simpul :")
+	// 		graph.PrintListNodes(nodes)
+	// 	}
+	// 	fmt.Print("Masukkan Nama Posisi AWal : ")
+	// 	var start string
+	// 	fmt.Scanln(&start)
+	// 	fmt.Print("Masukkan Nama Posisi Tujuan : ")
+	// 	var goal string
+	// 	fmt.Scanln(&goal)
+	// 	fmt.Println(graf.Astar(start, goal))
+	// }
 	//--------------------------------------
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("anajya ajnajnajibauhb"))
 }
