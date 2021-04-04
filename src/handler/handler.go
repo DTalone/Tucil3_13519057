9b2385ce25858eb4725b9fcb9d548d820d7e7e1f
DTalone <farisaziz22@gmail.com>
2021-04-04 10:50:35 +0700

    go run .

--- src/handler/handler.go
+++ src/handler/handler.go
@@ -1,26 +1,49 @@
 package handler
 
 import (
+	"html/template"
 	"log"
 	"net/http"
-	"path"
-	"html/template"
-
 	// "example.com/graph"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request){
+func Start() {
+	mux := http.NewServeMux()
 
-	tmpl, err := template.ParseFiles(path.Join("views", "index.html"))
-	if err != nil{
-		http.Error(w, "Error", http.StatusInternalServerError)
+	mux.HandleFunc("/", homeHandler)
+	mux.HandleFunc("/Hello", helloHandler)
+
+	log.Println("starting port 8080")
+
+	err := http.ListenAndServe(":8080", mux)
+
+	log.Fatal(err)
+}
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf(r.URL.Path)
+
+	if r.URL.Path != "/" {
+
+		http.NotFound(w, r)
 		return
 	}
 
+	tmpl, err := template.ParseFiles("../views/index.html")
+	log.Println(tmpl)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Error", http.StatusInternalServerError)
+		return
+	}
 
 	err = tmpl.Execute(w, nil)
-	if err != nil{
+	if err != nil {
+		log.Println(err)
 		http.Error(w, "Error", http.StatusInternalServerError)
 		return
 	}
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("anajya ajnajnajibauhb"))
 }
--- src/main/main.go
+++ src/main/main.go
@@ -1,78 +1,36 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"html/template"
-	// "example.com/handler"
-	// "example.com/graph"
+	"fmt"
+
+	"example.com/graph"
+	"example.com/handler"
 )
 
 func main() {
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/", homeHandler)
-	mux.HandleFunc("/Hello", helloHandler)
-
-	log.Println("starting port 8080")
-
-	err := http.ListenAndServe(":8080", mux)
-
-	log.Fatal(err)
-
-
-	// fmt.Println("Hello, World!")
-	// fmt.Println("Selamat datang di aplikasi A*")
-	// fmt.Print("Masukkan nama  file : ")
-	// var fileName string
-	// fmt.Scanln(&fileName)
-	// graf := graph.ReadFile(fileName)
-	// nodes := graf.GetNodes()
+	fmt.Println("Hello, World!")
+	fmt.Println("Selamat datang di aplikasi A*")
+	fmt.Print("Masukkan nama  file : ")
+	var fileName string
+	fmt.Scanln(&fileName)
+	graf := graph.ReadFile(fileName)
+	nodes := graf.GetNodes()
 	// for true {
-	// 	fmt.Print("Tampilkan Daftar Nama Simpul ? (y/n)")
-	// 	var command string
-	// 	fmt.Scanln(&command)
-	// 	if command == "y" {
-	// 		fmt.Println("Daftar Nama Simpul :")
-	// 		graph.PrintListNodes(nodes)
-	// 	}
-	// 	fmt.Print("Masukkan Nama Posisi AWal : ")
-	// 	var start string
-	// 	fmt.Scanln(&start)
-	// 	fmt.Print("Masukkan Nama Posisi Tujuan : ")
-	// 	var goal string
-	// 	fmt.Scanln(&goal)
-	// 	fmt.Println(graf.Astar(start, goal))
+	fmt.Print("Tampilkan Daftar Nama Simpul ? (y/n)")
+	var command string
+	fmt.Scanln(&command)
+	if command == "y" {
+		fmt.Println("Daftar Nama Simpul :")
+		graph.PrintListNodes(nodes)
+	}
+	fmt.Print("Masukkan Nama Posisi AWal : ")
+	var start string
+	fmt.Scanln(&start)
+	fmt.Print("Masukkan Nama Posisi Tujuan : ")
+	var goal string
+	fmt.Scanln(&goal)
+	fmt.Println(graf.Astar(start, goal))
 	// }
 	//--------------------------------------
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request){
-	log.Printf(r.URL.Path)
-
-	if r.URL.Path != "/"{
-		
-		http.NotFound(w,r)
-		return
-	}
-
-	tmpl, err := template.ParseFiles("src/views/index.html")
-	log.Println(tmpl)
-	if err != nil{
-		log.Println(err)
-		http.Error(w, "Error", http.StatusInternalServerError)
-		return
-	}
-
-
-	err = tmpl.Execute(w, nil)
-	if err != nil{
-		log.Println(err)
-		http.Error(w, "Error", http.StatusInternalServerError)
-		return
-	}
-}
-
-func helloHandler(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte("anajya ajnajnajibauhb"))
+	handler.Start()
 }
